75755b1c3af29b3857a4eafc5d089e61f25d59f8
Connor Davis <the.blu.dev@gmail.com>
2020-09-17 19:50:06 -0700

    Added the two main files lol

---
+++ nus3append.py
@@ -0,0 +1,121 @@
+import sys
+import os
+import struct
+import mmap
+
+filepath = input("Enter the path of your file: ")
+
+def readu32le(file):
+    return struct.unpack(b"<I", file.read(4))[0]
+
+def get_sub_meta_size(file):
+    while file.tell() % 4 != 0:
+        file.seek(1, 1)
+    while readu32le(file) != 0x22E8:
+        pass
+    start_pos = file.tell()
+    break_counter = 0
+    while True:
+        val = readu32le(file)
+        if break_counter % 2 == 0:
+            if val == 0:
+                break_counter += 1
+            else:
+                break_counter = 0
+        else:
+            if val == 0xFFFFFFFF:
+                break_counter += 1
+            else:
+                break_counter = 0
+        if break_counter == 8:
+            break
+    return file.tell() - start_pos
+
+assert os.path.exists(filepath), "File not found: " + str(filepath)
+nus3bank = open(filepath, u'rb+')
+if nus3bank.read(4) != b"NUS3":
+    print("Invalid file.")
+    exit()
+size = readu32le(nus3bank)
+assert nus3bank.read(8) == b"BANKTOC ", u"Invalid file."
+tocSize = readu32le(nus3bank)
+contentCount = readu32le(nus3bank)
+offset = 0x14 + tocSize
+for i in range(contentCount):
+    content = nus3bank.read(4)
+    contentSize = readu32le(nus3bank)
+    if content == b"TONE":
+        toneOffset = offset
+        toneSize = contentSize
+        break
+    offset += 8 + contentSize
+nus3bank.seek(toneOffset)
+assert nus3bank.read(4) == b"TONE"
+assert readu32le(nus3bank) == toneSize
+toneCount = readu32le(nus3bank)
+append_name = input("Enter the name of the appended sound effect: ")
+assert str(append_name), u"Invalid sound effect name"
+comparable_name = input("Enter the name of a comparable sound effect (metadata): ")
+assert str(comparable_name), u"Invalid sound effect name"
+file_as_string = mmap.mmap(nus3bank.fileno(), 0, access=mmap.ACCESS_READ)
+comparable_offset = file_as_string.find(bytes(comparable_name, 'utf8'))
+assert comparable_offset != -1, u"Comparable sound effect not found in this file. Are you sure you spelled it right?"
+nus3bank.seek(comparable_offset)
+comparable_size = get_sub_meta_size(nus3bank)
+nus3bank.seek(comparable_offset)
+val = readu32le(nus3bank)
+while nus3bank.tell() % 4 != 0:
+    nus3bank.seek(1, 1)
+while readu32le(nus3bank) != 0x22E8:
+    pass
+comparable_meta_data = nus3bank.read(comparable_size)
+nus3bank.seek(toneOffset + 12 + (toneCount - 1) * 8)
+lastToneOffset = readu32le(nus3bank)
+lastToneSize = readu32le(nus3bank)
+newToneSize = comparable_size + 28 + len(append_name)
+if len(append_name) % 4 != 0:
+    newToneSize += 4 - (len(append_name) % 4)
+else:
+    newToneSize += 4
+nus3bank.seek(0)
+original_file = nus3bank.read()
+nus3bank.close()
+extension_index = filepath.find('.')
+if extension_index == -1:
+    nus3bank = open(filepath + "_appended", u'wb+')
+else:
+    nus3bank = open(filepath[:extension_index] + "_appended" + filepath[extension_index:], u'wb+')
+nus3bank.seek(0)
+nus3bank.write(b"NUS3")
+nus3bank.write(struct.pack(b"<I", size + newToneSize + 8))
+nus3bank.write(original_file[8:(toneOffset + 4)])
+nus3bank.write(struct.pack(b"<I", toneSize + newToneSize + 8))
+modified_count = struct.pack(b"<I", toneCount + 1);
+nus3bank.write(modified_count)
+counter = 0
+while counter < toneCount:
+    curToneOffset = struct.unpack(b"<I", original_file[(toneOffset + 12 + counter * 8):(toneOffset + 16 + counter * 8)])[0]
+    modToneOffset = struct.pack(b"<I", curToneOffset + 8)
+    nus3bank.write(modToneOffset)
+    nus3bank.write(original_file[(toneOffset + 16 + counter * 8):(toneOffset + 20 + counter * 8)])
+    counter += 1
+nus3bank.write(struct.pack(b"<I", lastToneOffset + lastToneSize + 8))
+nus3bank.write(struct.pack(b"<I", newToneSize))
+nus3bank.write(original_file[(toneOffset + 12 + toneCount * 8):(toneOffset + 8 + lastToneOffset + lastToneSize)])
+nus3bank.write(struct.pack(b"<I", 0))
+nus3bank.write(struct.pack(b"<I", 0x87E7FFFF)) #these three lines are common in all the meta data I've seen so far. /shrug
+nus3bank.write(struct.pack(b"<I", 0x000C981F))
+nus3bank.write(struct.pack(b"B", len(append_name) + 1))
+nus3bank.write(bytes(append_name, 'utf8'))
+counter = len(append_name) + 1
+while counter % 4 != 0:
+    nus3bank.write(struct.pack(b"B", 0))
+    counter += 1
+nus3bank.write(struct.pack(b"<I", 0))
+nus3bank.write(struct.pack(b"<I", 0x8))
+nus3bank.write(struct.pack(b"<I", 0))
+nus3bank.write(struct.pack(b"<I", 0x22E8))
+nus3bank.write(comparable_meta_data)
+nus3bank.write(original_file[(toneOffset + 8 + lastToneOffset + lastToneSize):])
+nus3bank.close()
+print("Added entry", toneCount, "to", filepath)
---
+++ sli_patch.py
@@ -0,0 +1,76 @@
+import zlib
+import sys
+import os
+import struct
+
+def readu64le(file):
+    return struct.unpack(b"<Q", file.read(8))[0]
+
+def readu32le(file):
+    return struct.unpack(b"<I", file.read(4))[0]
+
+def gen_hash40(label):
+    return (len(label) << 32) | zlib.crc32(bytes(label, 'utf8'))
+
+filepath = input("Enter the filepath to soundlabelinfo.sli: ")
+assert os.path.exists(filepath), "File not found: " + str(filepath)
+sli = open(filepath, u'rb+')
+if sli.read(3) != b"SLI":
+    print("Invalid file. No SLI header")
+    exit()
+sli.seek(4)
+if readu32le(sli) != 0x01:
+    print("Invalid file. Improper SLI header")
+    exit()
+sli.seek(8)
+count = readu32le(sli)
+new_sound_label = input("Enter the new sound label: ")
+new_hash = gen_hash40(new_sound_label)
+new_index = int(input("Enter the index of the new sound label in the NUS3Bank: "))
+comp_label = input("Enter a comparable sound label (from the same NUS3Bank): ")
+comp_hash = gen_hash40(comp_label)
+new_offset = 0
+comp_offset = 0
+sli.seek(0xC)
+for x in range(0, count):
+    sli.seek(0x10 * x + 0xC)
+    hash40 = readu64le(sli)
+    if hash40 == comp_hash:
+        comp_offset = x
+    elif hash40 == new_hash:
+        print("The new sound label already exists in the table.")
+        exit()
+    elif (hash40 > new_hash) and (new_offset == 0):
+        new_offset = x
+    if (comp_offset != 0) and (new_offset != 0):
+        break
+if comp_offset == 0:
+    print("The comparable sound label could not be found in the table.")
+    exit()
+if new_offset == 0:
+    new_offset = count
+sli.seek(0)
+original_data = sli.read()
+sli.close()
+comp_data = original_data[(0xC + 0x10 * comp_offset + 0x8):(0xC + 0x10 * comp_offset + 0xC)]
+extension_index = filepath.find('.')
+if extension_index == -1:
+    sli = open(filepath + "_appended", u'wb+')
+else:
+    sli = open(filepath[:extension_index] + "_appended" + filepath[extension_index:], u'wb+')
+sli.write(b"SLI")
+sli.write(struct.pack(b"B", 0))
+sli.write(struct.pack(b"<I", 0x1))
+sli.write(struct.pack(b"<I", count + 1))
+if new_offset == count:
+    sli.write(original_data[0xC:])
+    sli.write(struct.pack(b"<Q", new_hash))
+    sli.write(comp_data)
+    sli.write(struct.pack(b"<I", new_index))
+else:
+    sli.write(original_data[0xC:(0xC + 0x10 * new_offset)])
+    sli.write(struct.pack(b"<Q", new_hash))
+    sli.write(comp_data)
+    sli.write(struct.pack(b"<I", new_index))
+    sli.write(original_data[(0xC + 0x10 * new_offset):])
+print("Added sound label", new_sound_label, "(", hex(new_hash), ") to", filepath)
